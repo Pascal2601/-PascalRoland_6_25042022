14148aa547de68a38f9ea740219d06ea37bc7b31
Pascal Roland <Pascal2601@live.fr>
2022-05-17 10:24:55 +0200

    initialisation base de donnée, shema sauces

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,50 @@
+// configuration des variables d'environement
+const dotenv = require("dotenv");
+dotenv.config();
+
 // application express
 const express = require("express");
 
 const app = express();
 
+const mongoose = require("mongoose");
+
+const saucesRoutes = require("./routes/sauces");
+
+const path = require("path");
+
+const cors = require("cors");
+
+const helmet = require("helmet");
+
+// Connexions à mongoDB
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.woq9f.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+//middelware de configuration de cors
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(express.json());
+app.use(cors());
+// sécuriser les en-têtes HTPP
+app.use(helmet());
+
+//configuration de route sauces
+app.use("/api/sauces", saucesRoutes);
 module.exports = app;
---
+++ backend/routes/sauces.js
@@ -0,0 +1,7 @@
+const express = require("express");
+
+const saucesCtrl = require("../controllers/sauces");
+
+const router = express.Router();
+
+module.exports = router;
